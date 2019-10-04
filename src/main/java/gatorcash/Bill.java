cc2e579c5b3181aeaf3fbd87e6470f799e49cf4a
Douglas Luman <dluman@douglasjluman.com>
2019-10-03 20:22:13 -0400

    Updating solution.

--- src/main/java/gatorcash/Bill.java
+++ src/main/java/gatorcash/Bill.java
@@ -1,6 +1,9 @@
 package gatorcash;
 
+import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /** Creates bill object with payee, amount.
  *
@@ -10,15 +13,26 @@ public class Bill {
   
   public final String payee;
   public final double amount;
+  public final Date dueDate;
   
   /** Constructor.
    *
    * @param name Payee of bill
    * @param cost Amount of bill
    */
-  public Bill(String name, double cost) {
-    payee = name;
+  public Bill(String name, double cost, String date) {
+    payee = name.trim();
     amount = cost;
+    dueDate = new Date(date);
+  }
+  
+  /** Formats date in mm/dd/yy format.
+   *
+   */
+  public String fmtDate() {
+    Locale locale = Locale.getDefault();
+    DateFormat fmt = DateFormat.getDateInstance(DateFormat.SHORT,locale);
+    return fmt.format(dueDate);
   }
   
   /** Formats amount as currency.
--- src/main/java/gatorcash/GatorAccount.java
+++ src/main/java/gatorcash/GatorAccount.java
@@ -18,7 +18,7 @@ public class GatorAccount {
     // Get balanace from command line arguments
     double startingBalance = Double.parseDouble(args[0]);
     // Set up BankAccount
-    BankAccount gatorAccount = new BankAccount(startingBalance);
+    BankAccount account = new BankAccount(startingBalance);
     // Declare file and scanner objects.
     File billFile = null;
     Scanner scanner = null;
@@ -30,13 +30,14 @@ public class GatorAccount {
     }
     // Gather data
     double amount = scanner.nextDouble();
+    String date = scanner.next();
     String payee = scanner.nextLine();
     // Create bill
-    Bill hatBill = new Bill(payee,amount);
+    Bill bill = new Bill(payee,amount,date);
     // Transact account business
-    gatorAccount.debit(hatBill.amount);
+    account.debit(bill.amount);
     // Display final balance
-    System.out.print("Subtracting a " + hatBill.fmtAmount() + " bill from " + hatBill.payee 
-                     + " you have: " + gatorAccount.getBalance());
+    System.out.print("Subtracting a " + bill.fmtAmount() + " bill from " + bill.payee 
+                     + " (payable on " + bill.fmtDate() + ") you have: " + account.getBalance());
   }
 }
